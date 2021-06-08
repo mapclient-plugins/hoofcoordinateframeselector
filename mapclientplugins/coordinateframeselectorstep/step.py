cf5b120dcbc1459a620b9df78e936d998578f107
TimmyNic <tim.nicolas@outlook.com>
2021-06-08 14:04:40 +1200

    Updated ConfigureDialog() arguments.

--- mapclientplugins/coordinateframeselectorstep/step.py
+++ mapclientplugins/coordinateframeselectorstep/step.py
@@ -108,7 +108,7 @@ class CoordinateFrameSelectorStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         '''
-        dlg = ConfigureDialog()
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
